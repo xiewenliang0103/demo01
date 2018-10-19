ac38f0ebe9b31db87642e0433286350e88ae2dfa
xiexie <2350321464@qq.com>
2018-10-19 17:16:40 +0800

    thirdadd

--- src/demo01/Demo01.java
+++ src/demo01/Demo01.java
@@ -3,5 +3,8 @@ package demo01;
 public class Demo01 {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		int i = 2;
+		int j = 1;
+		System.out.println(i + j);
 	}
 }
